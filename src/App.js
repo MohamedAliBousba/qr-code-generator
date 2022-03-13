9164ed69bbb637c211a14d850faece29430e2d23
Mohamed Ali <im_bousba@esi.dz>
2022-03-13 16:24:38 +0100

    add generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import QrCode from "qrcode";
 
 function App() {
+  const [text, setText] = useState("");
+  const [generatedCode, setGeneratedCode] = useState();
+
+  const generate = () => {
+    QrCode.toDataURL(text).then(setGeneratedCode);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <div>
+        <label className="form-label">Text</label>
+        <input
+          value={text}
+          onChange={e => setText(e.target.value)}
+          type="text"
+          className="form-control"
+          placeholder="type anything..."
+        />
+      </div>
+
+      <button className="btn btn-primary my-4" onClick={generate}>
+        Generate
+      </button>
+
+      <div>{generatedCode && <img src={generatedCode} alt="" />}</div>
     </div>
   );
 }
